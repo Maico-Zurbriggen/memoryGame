153c0d3a4fd313648f8b59f825ce29d39f25dccc
Maico Gabriel Zurbriggen <zurbriggenmaico8@gmail.com>
2025-02-01 12:34:20 -0300

    Definicion de url base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/memoryGame/',
+  build: { 
+    rollupOptions: { 
+      output: { 
+        entryFileNames: '[name].js', 
+        chunkFileNames: '[name].js', 
+        assetFileNames: '[name][extname]', 
+      }, 
+    }, 
+  }, 
 })
