80ad1ccb088db022ad799e34bff1e31f7cf238f6
Maico Gabriel Zurbriggen <zurbriggenmaico8@gmail.com>
2025-02-03 09:22:34 -0300

    Pagina responsive utilizando como breakpoints 480px 768px 834px 1024px 1440px

--- src/components/EndScreen/EndScreen.jsx
+++ src/components/EndScreen/EndScreen.jsx
@@ -8,16 +8,18 @@ const EndScreen = ({ players, winner, restartPanel, restartAll }) => {
       <h2>End Of The Game</h2>
       <h3>{winner}</h3>
       <ResultsTable players={players} />
-      <Button
-        text="Play Again"
-        onClick={restartPanel}
-        id="restartPanelButton"
-      />
-      <Button
-        text="Return to Menu"
-        onClick={restartAll}
-        id="restartAllButton"
-      />
+      <div className="flex-center">
+        <Button
+          text="Play Again"
+          onClick={restartPanel}
+          id="restartPanelButton"
+        />
+        <Button
+          text="Return to Menu"
+          onClick={restartAll}
+          id="restartAllButton"
+        />
+      </div>
     </>
   );
 };
